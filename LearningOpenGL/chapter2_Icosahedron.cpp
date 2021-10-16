1c06ff192d16c8c445f7cb56b8f2de12d3180672
djfdmator <djfdmator@naver.com>
2021-10-16 20:12:11 +0900

    chapter2_icosahedron 3D view mode

---
+++ LearningOpenGL/chapter2_Icosahedron.cpp
@@ -0,0 +1,68 @@
+#include"glut.h"
+
+#define X .525731112119133606
+#define Z .850650808352039932
+
+void init(void)
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glShadeModel(GL_FLAT);
+}
+
+static GLfloat vdata[12][3] = {
+	{-X, 0.0, Z}, {X, 0.0, Z}, {-X, 0.0, -Z}, {X, 0.0, -Z},
+	{0.0, Z, X}, {0.0, Z, -X}, {0.0, -Z, X}, {0.0, -Z, -X},
+	{Z, X, 0.0}, {-Z, X, 0.0}, {Z, -X, 0.0}, {-Z, -X, 0.0}
+};
+
+static GLuint tindices[20][3] = {
+	{1, 4, 0}, {4, 9, 0}, {4, 5, 9}, {8, 5, 4}, {1, 8, 4},
+	{1, 10, 8}, {10, 3, 8}, {8, 3, 5}, {3, 2, 5}, {3, 7, 2},
+	{3, 10, 7}, {10, 6, 7}, {6, 11, 7}, {6, 0, 11}, {6, 1, 0},
+	{10, 1, 6}, {11, 0, 9}, {2, 11, 9}, {5, 2, 9}, {11, 2, 7},
+};
+
+void display(void)
+{
+	int i;
+
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0f, 1.0f, 1.0f);
+	glLoadIdentity();
+	gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+	glBegin(GL_TRIANGLES);
+	for (i = 0; i < 20; i++)
+	{
+		glVertex3fv(&vdata[tindices[i][0]][0]);
+		glVertex3fv(&vdata[tindices[i][1]][0]);
+		glVertex3fv(&vdata[tindices[i][2]][0]);
+	}
+	glEnd();
+
+	glFlush();
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//gluOrtho2D(0.0, (GLdouble)w, 0.0, (GLdouble)h);
+	glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(800, 350);
+	glutInitWindowPosition(200, 200);
+	glutCreateWindow(argv[0]);
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+	return 0;
+}
