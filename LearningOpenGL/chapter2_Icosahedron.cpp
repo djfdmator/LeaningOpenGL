7f05fa6de10f5b39fa006e0fdbd251c2547252e4
djfdmator <djfdmator@naver.com>
2021-10-17 15:57:19 +0900

    subdivide object

--- LearningOpenGL/chapter2_Icosahedron.cpp
+++ LearningOpenGL/chapter2_Icosahedron.cpp
@@ -39,6 +39,62 @@ static GLuint tindices[20][3] = {
 	{10, 1, 6}, {11, 0, 9}, {2, 11, 9}, {5, 2, 9}, {11, 2, 7},
 };
 
+void drawtriangle(float *v1, float *v2, float *v3)
+{
+	glBegin(GL_TRIANGLES);
+	glNormal3fv(v1);
+	glVertex3fv(v1);	
+	glNormal3fv(v2);
+	glVertex3fv(v2);
+	glNormal3fv(v3);
+	glVertex3fv(v3);
+	glEnd();
+}
+
+void subdivide(float *v1, float *v2, float *v3)
+{
+	GLfloat v12[3], v23[3], v31[3];
+	GLint i;
+	for (i = 0; i < 3; i++)
+	{
+		v12[i] = (v1[i] + v2[i]) / 2.0;
+		v23[i] = (v2[i] + v3[i]) / 2.0;
+		v31[i] = (v3[i] + v1[i]) / 2.0;
+	}
+	normalize(v12);
+	normalize(v23);
+	normalize(v31);
+	drawtriangle(v1, v12, v31);
+	drawtriangle(v2, v23, v12);
+	drawtriangle(v3, v31, v23);
+	drawtriangle(v12, v23, v31);
+}
+
+void subdivide(float *v1, float *v2, float *v3, long depth)
+{
+	GLfloat v12[3], v23[3], v31[3];
+	GLint i;
+
+	if (depth == 0)
+	{
+		drawtriangle(v1, v2, v3);
+		return;
+	}
+	for (i = 0; i < 3; i++)
+	{
+		v12[i] = (v1[i] + v2[i]) / 2.0;
+		v23[i] = (v2[i] + v3[i]) / 2.0;
+		v31[i] = (v3[i] + v1[i]) / 2.0;
+	}
+	normalize(v12);
+	normalize(v23);
+	normalize(v31);
+	subdivide(v1, v12, v31, depth - 1);
+	subdivide(v2, v23, v12, depth - 1);
+	subdivide(v3, v31, v23, depth - 1);
+	subdivide(v12, v23, v31, depth - 1);
+}
+
 void init(void)
 {
 	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
@@ -80,20 +136,22 @@ void display(void)
 	//glBegin(GL_TRIANGLES);
 	for (i = 0; i < 20; i++)
 	{
-		GLfloat d1[3], d2[3], norm[3];
-		for (int j = 0; j < 3; j++)
-		{
-			d1[j] = vdata[tindices[i][0]][j] - vdata[tindices[i][1]][j];
-			d2[j] = vdata[tindices[i][1]][j] - vdata[tindices[i][2]][j];
-		}
-		normcrossprod(d1, d2, norm);
-		glNormal3fv(norm);
+		//GLfloat d1[3], d2[3], norm[3];
+		//for (int j = 0; j < 3; j++)
+		//{
+		//	d1[j] = vdata[tindices[i][0]][j] - vdata[tindices[i][1]][j];
+		//	d2[j] = vdata[tindices[i][1]][j] - vdata[tindices[i][2]][j];
+		//}
+		//normcrossprod(d1, d2, norm);
+		//glNormal3fv(norm);
 
 		//glArrayElement(tindices[i][1]);
 		//glArrayElement(tindices[i][2]);
 		//glArrayElement(tindices[i][0]);
 
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, tindices[i]);
+		//glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, tindices[i]);
+
+		subdivide(vdata[tindices[i][0]], vdata[tindices[i][1]], vdata[tindices[i][2]], 1);
 	}
 	//glEnd();
 	
